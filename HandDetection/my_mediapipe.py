d56fe8d1841ec05cbadef5a4398e98c22b30353d
Jdmg1308 <juandiegomendezgutierrez@gmail.com>
2023-10-21 13:34:39 -0500

    one file

---
+++ HandDetection/my_mediapipe.py
@@ -0,0 +1,109 @@
+#@markdown We implemented some functions to visualize the hand landmark detection results. <br/> Run the following cell to activate the functions.
+
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import cv2
+import time
+import mediapipe as mp
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      solutions.drawing_styles.get_default_hand_landmarks_style(),
+      solutions.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+    height, width, _ = annotated_image.shape
+    x_coordinates = [landmark.x for landmark in hand_landmarks]
+    y_coordinates = [landmark.y for landmark in hand_landmarks]
+    text_x = int(min(x_coordinates) * width)
+    text_y = int(min(y_coordinates) * height) - MARGIN
+
+    # Draw handedness (left or right hand) on the image.
+    cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+  return annotated_image
+
+width = 1080
+height = 720
+cap = cv2.VideoCapture(0)
+
+cap.set(3, width)
+cap.set(4, height)
+
+p_time = 0
+
+
+while True:
+    success, img = cap.read()
+
+    c_time = time.time()
+
+    cv2.imshow("img", img)
+    
+    fps = 1 / (c_time - p_time)
+
+    p_time = c_time
+
+    cv2.putText(img, f'FPS: {int(fps)},', (40, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 3)
+
+    cv2.waitKey(1)
+
+
+# from google.colab import files
+# uploaded = files.upload()
+
+# for filename in uploaded:
+#   content = uploaded[filename]
+#   with open(filename, 'wb') as f:
+#     f.write(content)
+
+# if len(uploaded.keys()):
+#   IMAGE_FILE = next(iter(uploaded))
+#   print('Uploaded file:', IMAGE_FILE)
+
+# Here are the steps to run hand landmark detection using MediaPipe.
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an HandLandmarker object.
+base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file("image.jpg")
+
+# STEP 4: Detect hand landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the classification result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
