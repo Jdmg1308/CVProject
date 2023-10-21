9a875407b62623bef404d2c8e3d3f99000f2c0b6
Nathan <nathannatedoghernandez@gmail.com>
2023-10-21 13:32:06 -0500

    hand cam works needs landmarks

--- HandDetection/Hand.py
+++ HandDetection/Hand.py
@@ -16,6 +16,8 @@ import time
 import mediapipe as mp
 
 
+
+
 width = 1080
 height = 720
 cap = cv2.VideoCapture(0)
@@ -25,14 +27,19 @@ cap.set(4, height)
 
 p_time = 0
 
+
 while True:
     success, img = cap.read()
 
     c_time = time.time()
+
+    cv2.imshow("img", img)
     
     fps = 1 / (c_time - p_time)
 
     p_time = c_time
 
     cv2.putText(img, f'FPS: {int(fps)},', (40, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 3)
+
+    cv2.waitKey(1)
 
