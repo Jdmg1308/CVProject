ec02530a32cd2b92eb72efbe5e685c8dd5597458
Jdmg1308 <juandiegomendezgutierrez@gmail.com>
2023-10-21 12:47:37 -0500

    Hand.py

---
+++ HandDetection/Hand.py
@@ -0,0 +1,23 @@
+
+
+## RAAAAWWWRRRRRRRRR *EAGLE* *EAGLE* *EAGLE* *EAGLE* *EAGLE* *EAGLE*  merica
+
+
+# what does import cv2 do?
+    # does the video capture
+        # while loop to continue the capture
+    # .read, .imshow, .waitkey
+        #.read - captures the img
+        #.imshow - shows the image
+        #.waitkey - adds a delay before the next capture
+
+# what does time do?
+
+# what does mediapipe do?
+
+#do we need to install packages?
+
+
+#import this as a module to our project
+    # import hand.py? chatgpt it
+
