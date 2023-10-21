73e7a6644c3610447caeb2a78d6ac5834d2a012a
Jdmg1308 <juandiegomendezgutierrez@gmail.com>
2023-10-21 16:04:02 -0500

    changes

--- HandDetection/Hand.py
+++ HandDetection/Hand.py
@@ -1,21 +1,6 @@
-
-
-## RAAAAWWWRRRRRRRRR *EAGLE* *EAGLE* *EAGLE* *EAGLE* *EAGLE* *EAGLE*  merica
-
-
-# what does import cv2 do?
-    # does the video capture
-        # while loop to continue the capture
-    # .read, .imshow, .waitkey
-        #.read - captures the img
-        #.imshow - shows the image
-        #.waitkey - adds a delay before the next capture
-# https://prod.liveshare.vsengsaas.visualstudio.com/join?3191DFC56B03C3D14E973AF0FD04B073A8A
 import cv2
 import time
 import mediapipe as mp
-
-
 
 
 width = 1080
@@ -33,13 +18,13 @@ while True:
 
     c_time = time.time()
 
-    cv2.imshow("img", img)
     
     fps = 1 / (c_time - p_time)
 
     p_time = c_time
 
     cv2.putText(img, f'FPS: {int(fps)},', (40, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 3)
+    cv2.imshow("img", img)
 
     cv2.waitKey(1)
 
--- HandDetection/my_mediapipe.py
+++ HandDetection/my_mediapipe.py
@@ -1,52 +1,83 @@
-#@markdown We implemented some functions to visualize the hand landmark detection results. <br/> Run the following cell to activate the functions.
+
+# @markdown We implemented some functions to visualize the hand landmark detection results. <br/> Run the following cell to activate the functions.
 
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import cv2
 import time
-import mediapipe as mp
+
 
 MARGIN = 10  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
 
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
 
-    # Draw the hand landmarks.
-    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    hand_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      hand_landmarks_proto,
-      solutions.hands.HAND_CONNECTIONS,
-      solutions.drawing_styles.get_default_hand_landmarks_style(),
-      solutions.drawing_styles.get_default_hand_connections_style())
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS,
+            solutions.drawing_styles.get_default_hand_landmarks_style(),
+            solutions.drawing_styles.get_default_hand_connections_style())
 
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
 
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
-  return annotated_image
+    return annotated_image
+
+# Here are the steps to run hand landmark detection using MediaPipe.
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# # STEP 2: Create an HandLandmarker object.
+# base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+# options = vision.HandLandmarkerOptions(base_options=base_options,
+#                                        num_hands=2)
+# detector = vision.HandLandmarker.create_from_options(options)
+
+# # STEP 3: Load the input image.
+# image = mp.Image.create_from_file("image.jpg")
+
+# # STEP 4: Detect hand landmarks from the input image.
+# detection_result = detector.detect(image)
+
+# # STEP 5: Process the classification result. In this case, visualize it.
+# annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+# cv2.imshow("image", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+
+import cv2
+import time
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
 
 width = 1080
 height = 720
@@ -63,7 +94,6 @@ while True:
 
     c_time = time.time()
 
-    cv2.imshow("img", img)
     
     fps = 1 / (c_time - p_time)
 
@@ -71,39 +101,21 @@ while True:
 
     cv2.putText(img, f'FPS: {int(fps)},', (40, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 3)
 
+    # STEP 2: Create an HandLandmarker object.
+    base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+    detector = vision.HandLandmarker.create_from_options(options)
+
+    # STEP 3: Load the input image.
+    image = mp.Image.create_from_file("image.jpg")
+
+    # STEP 4: Detect hand landmarks from the input image.
+    detection_result = detector.detect(image)
+
+    # STEP 5: Process the classification result. In this case, visualize it.
+    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+    cv2.imshow("image", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    # cv2.imshow("img", img)
+
     cv2.waitKey(1)
 
-
-# from google.colab import files
-# uploaded = files.upload()
-
-# for filename in uploaded:
-#   content = uploaded[filename]
-#   with open(filename, 'wb') as f:
-#     f.write(content)
-
-# if len(uploaded.keys()):
-#   IMAGE_FILE = next(iter(uploaded))
-#   print('Uploaded file:', IMAGE_FILE)
-
-# Here are the steps to run hand landmark detection using MediaPipe.
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-
-# STEP 2: Create an HandLandmarker object.
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
-
-# STEP 3: Load the input image.
-image = mp.Image.create_from_file("image.jpg")
-
-# STEP 4: Detect hand landmarks from the input image.
-detection_result = detector.detect(image)
-
-# STEP 5: Process the classification result. In this case, visualize it.
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-cv2_imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
