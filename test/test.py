3d0e3b6f3c02535881c6861eac7e972e641816e4
Jdmg1308 <juandiegomendezgutierrez@gmail.com>
2023-10-21 20:36:31 -0500

    pissss

--- test/test.py
+++ test/test.py
@@ -1,32 +1,79 @@
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
 import cv2
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+from mediapipe import solutions
 
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+        pose_landmarks_list = detection_result.pose_landmarks
+        #pose_landmarks_list = detection_result
+        annotated_image = np.copy(rgb_image)
+
+        # Loop through the detected poses to visualize.
+        for idx in range(len(pose_landmarks_list)):
+            pose_landmarks = pose_landmarks_list[idx]
+
+            # Draw the pose landmarks.
+            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
+video = cv2.VideoCapture(0)
 
-# Create a gesture recognizer instance with the video mode:
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    my_result = result
+    print('gesture recognition result: {}'.format(result))
+
+
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
-    running_mode=VisionRunningMode.VIDEO)
-with GestureRecognizer.create_from_options(options) as recognizer:
-    mp_drawing = mp.solutions.drawing_utils
-    mp_drawing_styles = mp.solutions.drawing_styles
-    mphands = mp.solutions.hands
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
 
-    cap = cv2.VideoCapture(0)
-    hands = mphands.Hands()
-    while True:
-        data, image=cap.read()
+timestamp = 0
+with GestureRecognizer.create_from_options(options) as recognizer:
+  # The recognizer is initialized. Use it here.
+    while video.isOpened(): 
+        # Capture frame-by-frame
+        ret, frame = video.read()
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
-        detection_result = recognizer.detect(results)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand_landmarks, mphands.HAND_CONNECTIONS)
-        cv2.imshow("Handtracker", image)
-        cv2.waitKey(1)
+    results = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            for landmark in hand_landmarks.landmark:
+                x, y = int(landmark.x * image.shape[1]), int(landmark.y * image.shape[0])
+                cv2.circle(image, (x, y), 8, (0, 0, 0), -1)  # black color (BGR format)
+                cv2.circle(image, (x, y), 6, (255, 255, 255), -1)  # white color (BGR format)
+
+            for connection in mphands.HAND_CONNECTIONS:
+                x0, y0 = int(hand_landmarks.landmark[connection[0]].x * image.shape[1]), int(hand_landmarks.landmark[connection[0]].y * image.shape[0])
+                x1, y1 = int(hand_landmarks.landmark[connection[1]].x * image.shape[1]), int(hand_landmarks.landmark[connection[1]].y * image.shape[0])
+                cv2.line(image, (x0, y0), (x1, y1), (0, 0, 0), 6)  # black color (BGR format)
+
+                cv2.line(image, (x0, y0), (x1, y1), (255, 255, 255), 4)  # white color (BGR format)
+
+    #cv2.namedWindow("Handtracker", cv2.WINDOW_NORMAL)
+    cv2.imshow("Handtracker", image)
+    #cv2.resizeWindow("Handtracker", window_width, window_height)
+        if cv2.waitKey(5) & 0xFF == 'q':
+            break
+
+video.release()
+cv2.destroyAllWindows()
